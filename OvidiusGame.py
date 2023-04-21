3c037f8eb9108f8c53b15d2ef0b5ca61d8c46e83
vietduc030 <128697747+vietduc030@users.noreply.github.com>
2023-04-21 08:34:57 +0200

    Update OvidiusGame.py

--- OvidiusGame.py
+++ OvidiusGame.py
@@ -16,10 +16,11 @@ number_rider2=1
 player1=[]
 player2=[]
 x=0
+wrong=True
 
 while(king1==True and king2==True): 
     
-    print(f"\n\nLets play round {x+1}: ")
+    print(f"!LETS PLAY ROUND {x+1}: ")
     print("Which Unit do you want to choose?")
     
     #GENERATE NUMBER FOR PLAYER 1
@@ -27,25 +28,38 @@ while(king1==True and king2==True):
      print("Out of Soldier Units. You will choose a Rider this Round")
      player1=rider()
     elif(number_rider1==0 and number_soldier1>0): #if Out of riders but still Soldiers
-       print("Out of Rider Units. You will choose a Solduer this Round")
+       print("Out of Rider Units. You will choose a Soldier this Round")
        player1=soldier()
     else:
-       unit=input("Choose Your Unit!\nEnter 'soldier' or 'rider': ")
-       if(unit=="soldier"): 
-          player1=soldier()
-       if(unit=="rider"):
-          player1=rider()
+       
+       while(wrong==True):    ##TYPE AGAIN IF WRONG INPUT
+         unit=input("Choose Your Unit!\nEnter 'soldier' or 'rider': ") #If you have at least 1 soldier and 1  rider
+         if(unit=="soldier"): 
+            player1=soldier()
+            wrong=False
+            
+         elif(unit=="rider"):
+            player1=rider()
+            wrong=False
+         else:
+            wrong=input("!!!WRONG INPUT!!! Type Again: ")
+            wrong=True
+         
     print("\nYou rolled ",player1[0])
+    wrong=True
+
 
      #GENERATE NUMBER FOR PLAYER 2
     if(number_soldier2==0 and number_rider2>0):  #If out of soldiers but still riders
-     print("Out of Soldiers. Player2 Chooses a Rider!")
+     print("Out of Soldiers. Player2 Chooses a Rider!",end=" ")
      player2=rider()
+     print("He rolled a ",player2[0])
     elif(number_rider2==0 and number_soldier2>0): #if Out of riders but still Soldiers
-       print("Out of Rider Units. Player2 Chooses a Soldier this Round")
+       print("Out of Rider Units. Player2 Chooses a Soldier this Round",end=" ")
        player2=soldier()
+       print("He rolled a ",player2[0])
     else:
-       luck=random.randint(1,2)
+       luck=random.randint(1,2)  #50% Chance to Use either Soldier or Rider
        if(luck==1): 
           player2=soldier()
           print("Player 2 Chooses a Soldier. He rolled: ",player2[0])
@@ -63,9 +77,6 @@ while(king1==True and king2==True):
           number_rider1+=1   #if you won and enemy used rider
           number_rider2-=1
           
-    
-        
-    
     if(player1[0]<player2[0]): #If your score is lower
        print("You lost this round")
        if(player1[1]=="s"):   #If you lost with soldier
@@ -75,9 +86,8 @@ while(king1==True and king2==True):
           number_rider1-=1   #if you lost with rider
           number_rider2+=1
     
-    
     print("Remaining Units:\nPlayer1:",number_soldier1,"Soldiers and",number_rider1 ,"Riders")
-    print("Player2:",number_soldier2,"Soldiers and",number_rider2 ,"Riders")
+    print("Player2:",number_soldier2,"Soldiers and",number_rider2 ,"Riders\n------------------------")
     x+=1
     
     if(number_rider1+number_soldier1==0):
@@ -86,5 +96,8 @@ while(king1==True and king2==True):
        king2=False
 
 
-    
 print("Game Ended")
+if(king1==True):
+   print("WINNER!")
+else:
+   print("LOOSER!")
